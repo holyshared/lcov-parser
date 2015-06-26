ffd07d1d1da02768792b4c4d2ff134dfa01a91a6
holyshared <holy.shared.design@gmail.com>
2015-06-26 20:40:53 +0900

    add parser

--- src/parser.rs
+++ src/parser.rs
@@ -2,37 +2,39 @@ use LcovRecord;
 use combinator:: { parse_record };
 use nom:: { IResult };
 use lines::linereader:: { LineReader };
-use std::io:: { Read };
+use std::io:: { Read, Error, ErrorKind };
 use std::fs:: { File };
 use std::str::{ from_utf8 };
 
 pub struct RecordParser<R> {
-    reader: LineReader<R>,
-    records: Vec<LcovRecord>
+    reader: LineReader<R>
+}
+
+pub trait RecordReceiver {
+    fn receive(&mut self, result : &Result<LcovRecord, &str>);
 }
 
 impl<R: Read> RecordParser<R> {
     fn new(reader: R) -> Self {
         RecordParser {
-            reader: LineReader::new(reader),
-            records: vec!()
+            reader: LineReader::new(reader)
         }
     }
-    fn parse(&mut self) {
+    fn parse<P: RecordReceiver>(&mut self, receiver: &mut P) {
         loop {
-            match self.reader.read_line() {
-                Ok(b) if b.is_empty() => { break; }
+            let result = self.reader.read_line();
+            let line = match result {
+                Ok(b) if b.is_empty() => { break; },
                 Ok(line) => {
-                    match parse_record(line) {
-                        IResult::Done(_, output) => self.records.push(output),
-                        IResult::Error(a) => println!("{:?}", a),
-                        IResult::Incomplete(needed) => continue
-                    }
+                    let r = match parse_record(line) {
+                        IResult::Done(_, output) => Result::Ok(output),
+                        IResult::Error(a) => Result::Err("dff"),
+                        IResult::Incomplete(_) => Result::Err("aa")
+                    };
+                    receiver.receive(&r);
                 }
-                Err(e) => {
-                    break;
-                }
-            }
+                Err(e) => { break; }
+            };
         }
     }
 }
@@ -44,13 +46,24 @@ mod tests {
     use combinator:: { parse_record };
     use std::fs::File;
 
+    struct TestReceiver {
+        record_count: u32
+    }
+
+    impl RecordReceiver for TestReceiver {
+        fn receive(&mut self, result: &Result<LcovRecord, &str>) {
+            self.record_count = self.record_count + 1;
+        }
+    }
+
     #[test]
     fn test_parse_from_file() {
+        let mut r = TestReceiver { record_count: 0 };
         let mut f = File::open("./fixture/report.lcov").unwrap();
         let mut parser = RecordParser::new(f);
 
-        parser.parse();
+        parser.parse(&mut r);
 
-        assert_eq!(parser.records.len(), 1);
+        assert_eq!(r.record_count, 1);
     }
 }
