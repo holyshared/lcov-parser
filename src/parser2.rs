33693c0139ecc961600f01911d5fa337c4fa1bc5
holyshared <holy.shared.design@gmail.com>
2015-07-08 19:37:42 +0900

    add parse function

--- src/parser2.rs
+++ src/parser2.rs
@@ -29,6 +29,29 @@ pub fn parse_record2(input: &[u8]) -> Result<LCOVRecord> {
     }
 }
 
+/// parse all the records
+///
+/// # Examples
+///
+/// ```
+/// use lcov_parser:: { LCOVRecord, parse_all_records2 };
+///
+/// let result = parse_all_records2(b"TN:test_name\n");
+///
+/// assert_eq!(result.unwrap(), vec!( LCOVRecord::TestName("test_name".to_string())));
+/// ```
+pub fn parse_all_records2(input: &[u8]) -> Result<Vec<LCOVRecord>> {
+    match from_utf8(input) {
+        Ok(value) => {
+            match parser(records).parse(value) {
+                Ok((records, _)) => Ok(records),
+                Err(error) => Err(Error::new(ErrorKind::InvalidInput, format!("{}", error)))
+            }
+        },
+        Err(error) => Err(Error::new(ErrorKind::InvalidInput, format!("{}", error)))
+    }
+}
+
 fn parse_for_record(input: &str) -> Result<LCOVRecord> {
     let parsed_result = parser(record).parse(input);
 
