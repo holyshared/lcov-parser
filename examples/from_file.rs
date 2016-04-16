9cdd62eaa6ea905c76ce4f1ad57e229c9e94c86b
holyshared <holy.shared.design@gmail.com>
2016-04-16 19:52:34 +0900

    Update example

--- examples/from_file.rs
+++ examples/from_file.rs
@@ -1,31 +1,12 @@
 extern crate lcov_parser;
 
 use lcov_parser:: { LCOVParser };
-use std::fs:: { File };
-use std::io:: { Read, ErrorKind };
-use std::env:: { current_dir };
 
 fn main() {
-    let current_dir = current_dir().unwrap();
-    let file = current_dir.join("../../../fixture/report.lcov");
-    let read_file_path = file.clone();
+    let parser = LCOVParser::from("../../../fixture/report.lcov");
+    let records = parser.parse().unwrap();
 
-    match File::open(file) {
-        Ok(ref mut file) => {
-            let mut buffer = String::new();
-            let _ = file.read_to_string(&mut buffer);
-            let records = LCOVParser::new(buffer.as_str()).parse().unwrap();
-
-            for record in records.iter() {
-                println!("{:?}", record);
-            }
-        },
-        Err(error) => {
-            match error.kind() {
-                ErrorKind::NotFound => panic!("File not found: {:?}", read_file_path),
-                ErrorKind::PermissionDenied => panic!("Permission denied"),
-                _ => panic!("{:?}", error)
-            }
-        }
+    for record in records.iter() {
+        println!("{:?}", record);
     }
 }
